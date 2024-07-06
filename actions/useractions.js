a974b1d2db7e5830a4bb9f80255c0efd6c243949
Anuj <anujverma3553@gmail.com>
2024-07-06 10:24:44 +0530

    Some Fix Issue

--- actions/useractions.js
+++ actions/useractions.js
@@ -1,111 +1,120 @@
 "use server"
 
-import Razorpay from "razorpay";
-import Payment from "@/models/Payment";
-import connectDB from "@/db/connectDB";
-import User from "@/models/User";
-import Subscription from "@/models/Subscription";
+import Razorpay from "razorpay"
+import Payment from "@/models/Payment"
+import connectDB from "@/db/connectDB"
+import User from "@/models/User"
+import Subscription from "@/models/Subscription"
 
 export const initiate = async (amount, to_username, paymentform) => {
-    await connectDB();
+    await connectDB()
 
-    let user = await User.findOne({ username: to_username }).lean();
-    const secret = user.razorpaysecret;
+    // Fetch the secret of the user who is getting the payment
+    let user = await User.findOne({ username: to_username })
+    const secret = user.razorpaysecret
 
-    const instance = new Razorpay({ key_id: user.razorpayid, key_secret: secret });
+    var instance = new Razorpay({ key_id: user.razorpayid, key_secret: secret })
 
-    const options = {
+    let options = {
         amount: Number.parseInt(amount),
         currency: "INR",
-    };
+    }
 
-    const x = await instance.orders.create(options);
+    let x = await instance.orders.create(options)
 
-    await Payment.create({ oid: x.id, amount: amount / 100, to_user: to_username, name: paymentform.name, message: paymentform.message });
+    // create a payment object which shows a pending payment in the database
+    await Payment.create({ oid: x.id, amount: amount / 100, to_user: to_username, name: paymentform.name, message: paymentform.message })
 
     return x;
-};
+}
+
 
 export const fetchuser = async (username) => {
     await connectDB();
 
-    const user = await User.findOne({ username }).lean();
+    let u = await User.findOne({ username: username });
+    let user = u.toObject({ flattenObjectIds: true });
     return user;
-};
+}
 
 export const fetchProfile = async () => {
     await connectDB();
 
-    const users = await User.find({}).limit(4).lean();
-    return users;
-};
+    const user = await User.find({}).limit(4);
+    return user;
+}
 
 export const fetchPaymentData = async () => {
     await connectDB();
 
-    const payments = await Payment.find({}).lean();
+    const payments = await Payment.find({});
     return payments;
-};
+}
 
 export const fetchpayments = async (username) => {
-    await connectDB();
+    await connectDB()
 
-    const payments = await Payment.find({ to_user: username, done: true }).sort({ amount: -1 }).lean();
-    return payments;
-};
+    // Find all payments sorted by decreasing order of amount and flatten object
+    let p = await Payment.find({ to_user: username, done: true }).sort({ amount: -1 }).lean()
+    return p
+}
 
 export const updateProfile = async (data, oldusername) => {
-    await connectDB();
+    await connectDB()
 
-    const ndata = Object.fromEntries(data);
-
+    let ndata = Object.fromEntries(data)
+    // If the username is being updated, check if username is available
     if (oldusername !== ndata.username) {
-        const userExists = await User.findOne({ username: ndata.username }).lean();
-        if (userExists) {
-            return { error: "Username already exists" };
+        let u = await User.findOne({ username: ndata.username })
+        if (u) {
+            return { error: "Username already exists" }
         }
-        await User.updateOne({ email: ndata.email }, ndata);
-        await Payment.updateMany({ to_user: oldusername }, { to_user: ndata.username });
-    } else {
-        await User.updateOne({ email: ndata.email }, ndata);
+        await User.updateOne({ email: ndata.email }, ndata)
+        // Now update all the usernames in the Payments table
+        await Payment.updateMany({ to_user: oldusername }, { to_user: ndata.username })
     }
-};
+    else {
+        await User.updateOne({ email: ndata.email }, ndata)
+    }
+}
 
 export async function UserCount() {
     await connectDB();
 
     try {
-        const userCount = await User.countDocuments().lean();
-        return userCount;
+        const userCount = await User.countDocuments();
+        return userCount
     } catch (error) {
-        return { error: 'Error fetching user count' };
+        res.status(500).json({ error: 'Error fetching user count' });
     }
 }
+
 
 export async function PaymentCount() {
     await connectDB();
 
     try {
-        const paymentCount = await Payment.countDocuments().lean();
-        return paymentCount;
+        const paymentCount = await Payment.countDocuments();
+        return paymentCount
     } catch (error) {
-        return { error: 'Error fetching payment count' };
+        res.status(500).json({ error: 'Error fetching payment count' });
     }
 }
+
 
 export async function SubscribeUser(formData) {
     await connectDB();
 
-    const email = formData.get('email');
+    const email = formData.get('email'); // Extract the 'email' field from FormData
 
     if (!email) {
         return { error: 'Email is required' };
     }
 
     try {
-        const newSubscription = new Subscription({ email });
+        let newSubscription = new Subscription({ email });
         await newSubscription.save();
-        return { message: 'Subscription successful', subscription: newSubscription.toObject() };
+        return { message: 'Subscription successful', subscription: newSubscription };
     } catch (error) {
         return { error: 'Failed to subscribe user' };
     }
--- app/components/HorizontalCarouse.js
+++ app/components/HorizontalCarouse.js
@@ -68,7 +68,7 @@ const HorizontalCarousel = () => {
                                 <p className="text-sm text-gray-600">Donation Streak</p>
                             </div> */}
                         </div>
-                        <Link href={`${process.env.URL}/${profile.username}`}>
+                        <Link href={`/${profile.username}`}>
                             <div className="p-4 border-t mx-8 mt-2 flex justify-center">
                                 <button className="md:w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Donate</button>
                             </div>
